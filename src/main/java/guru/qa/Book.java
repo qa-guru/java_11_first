12e11fea73fc5bf53c4aca45dbfa1d92e3a0a3e1
Dmitrii Tuchs <dtuchs@gmail.com>
2022-03-16 21:29:56 +0300

    loops & collections

--- src/main/java/guru/qa/Book.java
+++ src/main/java/guru/qa/Book.java
@@ -1,11 +1,31 @@
 package guru.qa;
 
+import java.util.Objects;
+
 public class Book {
 
-    String author = "Дима Тучс";
-    String text = "Мурзилка";
+    String author;
+    String text;
 
-    void doReadBook() {
-        System.out.println("книга прочитана! " + author);
+    public Book(String author, String text) {
+        this.author = author;
+        this.text = text;
+    }
+
+    String doReadBook() {
+        return "книга прочитана от автора: " + author + " с текстом: " + text;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(author, book.author) && Objects.equals(text, book.text);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(author, text);
     }
 }
--- src/main/java/guru/qa/FirstClass.java
+++ src/main/java/guru/qa/FirstClass.java
@@ -1,9 +1,14 @@
 package guru.qa;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class FirstClass {
 
     byte aByte; // 8 bit -128 ... 127
-    short aShort = 100 ; // 16 bit -32768 ... 32767
+    short aShort = 100; // 16 bit -32768 ... 32767
 
     // самый используемый
     int aInt = 1_100_000; // 32 bit -2 ^ 31 ... (2 ^ 31) -1
@@ -52,9 +57,62 @@ public class FirstClass {
 
         boolean result = (3 < 4) || false;
 
-        Book murzilka = new Book();
-        murzilka.doReadBook();
+//        Book war = new Book("Лев Толстой", "Война и Мир");
+        Book war0 = new Book("Лев Толстой", "Анна Каренина");
+        Book war1 = new Book("Лев Толстой", "Анна Каренина");
+        Book stupid = new Book("Федор Достоевский", "Идиот");
+        Book master = new Book("Михаил Булгаков", "Мастер и Маргарита");
+        Book remark = new Book("Эрих Мария Ремарк", "Ночь в Лиссабоне");
+
+
+        Book[] library = new Book[]{war0, war1, stupid, master, remark};
+        List<Book> libList = List.of(war0, war1, stupid, master, remark);
+        Set<Book> libSet = Set.of(war0, war1, stupid, master, remark);
+
+        Map<Integer, Book> booksMap = new HashMap<>() {{
+            put(1, war0);
+            put(2, war1);
+            put(3, stupid);
+        }};
+
+
+
+        for (int i = (libList.size() - 1); i >= 0; i--) {
+            System.out.println("book " + libList.get(i).doReadBook());
+        }
+
+        System.out.println("###");
+
+        for (Book book : libSet) {
+            System.out.println("book " + book.doReadBook());
+        }
+
+        int index = 0;
+        while (index < library.length) {
+            System.out.println("book " + library[index].doReadBook());
+            index++;
+        }
+
+        do {
+            System.out.println("book " + library[index].doReadBook());
+            index++;
+        } while (index < library.length);
+
+        Book desiredBook;
+
+        for (Book book : libList) {
+            if (book.author.contains("Достоевский")) {
+                desiredBook = book;
+            }
+        }
+
+        for (Map.Entry<Integer, Book> bookEntry : booksMap.entrySet()) {
+            Integer key = bookEntry.getKey();
+            Book value = bookEntry.getValue();
+        }
+
+        for (Book value : booksMap.values()) {
+
+        }
     }
-
-
 }
